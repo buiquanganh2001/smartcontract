e7d816a04456ce9ccdafbacc21f5e5508c23b57c
Bùi Q <buiquanganh2001@gmail.com>
2021-07-22 10:43:02 +0700

    first commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+
+contract Inbox {
+    string public message;
+    
+    function Inbox(string InitialMessage) public { 
+    
+        message = InitialMessage;
+        }
+    
+    function setMessage (string newMessage) public {
+        message = newMessage;
+    }
+    
+}
---
+++ deploy.js
@@ -0,0 +1,23 @@
+// deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  '12 memonic words',
+  'https://rinkeby.infura.io/v3/83cd193f708f4f8ba0aff75306afa1ed'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
